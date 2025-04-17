c2e7b777a5237a6d90792043c18583f7f5afcd82
Alexander Temp <alex@aewtemp.de>
2025-04-17 23:51:35 +0200

    updated config.py and config.toml.example

--- rpi_energy_meter/config.py
+++ rpi_energy_meter/config.py
@@ -1,61 +1,9 @@
 from typing import Dict, Optional, Union
-from pydantic import BaseModel, Field
 from pathlib import Path
 from box import Box
 from datetime import datetime
 import tomli
 import tomli_w
-
-
-class GeneralConfig(BaseModel):
-    VREF: float
-    ADC_RESOLUTION: int
-    ADC_SAMPLERATE: int
-    ADC_SAMPLES: int
-
-
-class InfluxConfig(BaseModel):
-    host: str
-    port: int
-    token: str
-    organization: str
-    bucket: str
-
-
-class PhaseDetails(BaseModel):
-    VOLTAGE: float
-    TRANSFORMER_OUTPUT_VOLTAGE: float
-
-
-class PhasesConfig(BaseModel):
-    COUNT: int
-    FREQUENCY: float
-    TRANSFORMER_VDIVIDER: int
-    details: Dict[str, PhaseDetails] = Field(default_factory=dict)
-
-
-class CTConfig(BaseModel):
-    DESCRIPTION: str
-    TYPE: str
-    SHIFT: float
-    FACTOR: float
-    CUTOFF: float
-
-
-class CTPhase(BaseModel):
-    channels: Dict[str, CTConfig] = Field(default_factory=dict)
-
-
-class CTsConfig(BaseModel):
-    BURDEN_RESISTANCE: float
-    phases: Dict[str, CTPhase] = Field(default_factory=dict)
-
-
-class FullConfig(BaseModel):
-    GENERAL: GeneralConfig
-    INFLUX: InfluxConfig
-    PHASES: PhasesConfig
-    CTS: CTsConfig
 
 def load_config(path: Union[str, Path]) -> Box:
     """
@@ -95,7 +43,7 @@ def read_total_kwh(config) -> list:
 def save_total_kwh(config, measurements):
     for phase in range(config.PHASES.COUNT):
         for ct in range(config.CTS.get(str(phase + 1)).COUNT):
-            if config.CTS.get(str(phase + 1)).get(str(ct + 1)).get('RESET_UTC') == 0 or \
+            if config.CTS.get(str(phase + 1)).get(str(ct + 1)).get('RESET_UTC') == "1970-1-1 00:00:00.000000" or \
             float(config.CTS.get(str(phase + 1)).get(str(ct + 1)).KWH) > measurements[phase]._energy[ct]['Total']:
                 # config.CTS.get(str(phase + 1)).get(str(ct + 1)).set("RESET_UTC", str(datetime.utcnow()))
                 config.CTS[str(phase+1)][str(ct+1)].RESET_UTC = str(datetime.utcnow())
